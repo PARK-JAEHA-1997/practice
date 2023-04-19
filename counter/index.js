660f5f00edf8e17e126eb1ff7183288bb2c0db52
PARK JAEHA <parkjaeha97@gmail.com>
2023-04-19 13:34:34 +0900

    counter page with eventlistener

--- counter/index.js
+++ counter/index.js
@@ -2,7 +2,11 @@ let value = 0;
 
 document.querySelector("#value").innerHTML = value;
 
+let button = document.getElementsByClassName("CounterButton");
+button[0].addEventListener("click", () => up());
+
 function up() {
   value++;
   console.log(value);
+  document.querySelector("#value").innerHTML = value;
 }
